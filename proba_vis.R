cff3118ae9f56818d9d9659a388bf9a2e33e50e8
Kris Sankaran <sankaran.kris@gmail.com>
2019-01-11 20:08:48 -0500

    visualizing the multiview data, as functions

---
+++ proba_vis.R
@@ -0,0 +1,77 @@
+#' Visualizations of the Proba-V data
+#'
+
+library("tidyverse")
+library("reshape2")
+library("EBImage")
+theme_set(
+  theme_bw() +
+  theme(
+    panel.grid =  element_blank(),
+    panel.spacing = unit(0, "cm"),
+    axis.text.y =  element_blank(),
+    axis.ticks.y =  element_blank()
+  )
+)
+
+## read in all the data
+data_dir <- "/Users/krissankaran/Desktop/super-res/superres_data/train/NIR/"
+id <- "imgset1153"
+
+lr_files <- list.files(file.path(data_dir, id), "LR*", full.names = TRUE)
+hr_file <- list.files(file.path(data_dir, id), "HR*", full.names = TRUE)
+qm_files <- list.files(file.path(data_dir, id), "QM*", full.names = TRUE)
+sm_file <- list.files(file.path(data_dir, id), "SM*", full.names = TRUE)
+
+lr <- lapply(lr_files, readImage)
+qm <- lapply(qm_files, readImage)
+hr <- readImage(hr_file)
+sm <- readImage(sm_file)
+R <- 10 # number of rows to plot
+
+## reshape all the data into data frames
+qm_df <- melt(lapply(qm, function(x) x[1:R, ])) %>%
+  rename(quality = value) %>%
+  mutate(quality = as.factor(quality))
+
+lr_df <- melt(lapply(lr, function(x) x[1:R, ])) %>%
+  left_join(qm_df) %>%
+  mutate(
+    Var2 = seq(1, nrow(hr), length.out = nrow(lr[[1]]))[Var2]
+  ) %>%
+  rename(view = L1)
+
+sm_df <- melt(sm[1:R, ]) %>%
+  rename(quality = value) %>%
+  mutate(quality = as.factor(quality))
+
+hr_df <- melt(hr[1:R, ]) %>%
+  left_join(sm_df)
+
+## generate the plot
+ggplot() +
+  geom_point(
+    data = lr_df,
+    aes(
+      x = Var2,
+      y = value,
+      col = quality
+    ),
+    size = 0.05,
+    alpha = 0.2
+  ) +
+  geom_point(
+    data = hr_df,
+    aes(
+      x = Var2,
+      y = value,
+      col = quality
+    ),
+    size = 0.1,
+    alpha = 0.8
+  ) +
+  guides(colour = guide_legend(override.aes = list(alpha = 1, size = 4))) +
+  scale_color_manual(values = c("#cd5b45", "#5d3954")) +
+  facet_grid(Var1 ~ .)
+
+ggsave(sprintf("notes/figure/views_comparison_%s.png", id), width = 4, height = 7, dpi = 400)
