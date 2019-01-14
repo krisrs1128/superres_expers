4ccbad660b948a8187c9157f76a7fbbbb7ec4cb1
Kris Sankaran <sankaran.kris@gmail.com>
2019-01-14 18:58:34 -0500

    smarter patching

--- data.py
+++ data.py
@@ -16,57 +16,26 @@ class ProbaBase(Dataset):
     """
     Shared Paths for Proba Data
     """
-    def __init__(self, data_dir):
+    def __init__(self, data_dir, prefix="LR"):
         super(ProbaBase, self).__init__()
         self.data_dir = data_dir
-        self.imgsets = [os.path.join(data_dir, s) for s in os.listdir(self.data_dir)]
+        imgsets = [os.path.join(data_dir, s) for s in os.listdir(self.data_dir)][:25]
+        self.images = []
+
+        sites = [glob.glob(os.path.join(s, prefix + "*.png")) for s in imgsets]
+        for views in sites:
+            x = [transforms.ToTensor()(Image.open(s)) for s in views]
+            if len(x) == 1:
+                x = x[0]
+
+            self.images.append(x)
+
 
     def __len__(self):
-        return len(self.imgsets)
+        return len(self.images)
 
     def __getitem__(self, index):
-        return
-
-    def getitem_wrapper(self, index, prefix="LR"):
-        paths = glob.glob(os.path.join(self.imgsets[index], prefix + "*.png"))
-        result = [transforms.ToTensor()(Image.open(s)) for s in paths]
-        if len(result) == 1:
-            return result[0]
-
-        return result
-
-
-class QualityMaps(ProbaBase):
-    """
-    Quality Maps for Low Res Data
-    """
-    def __init__(self, data_dir):
-        super(QualityMaps, self).__init__(data_dir)
-
-    def __getitem__(self, index):
-        return self.getitem_wrapper(index, "QM")
-
-
-class LowRes(ProbaBase):
-    """
-    List of Low Res Views of a Site
-    """
-    def __init__(self, data_dir):
-        super(LowRes, self).__init__(data_dir)
-
-    def __getitem__(self, index):
-        return self.getitem_wrapper(index, "LR")
-
-
-class HighRes(ProbaBase):
-    """
-    High Res Image for a Site
-    """
-    def __init__(self, data_dir):
-        super(HighRes, self).__init__(data_dir)
-
-    def __getitem__(self, index):
-        return self.getitem_wrapper(index, "HR")
+        return self.images[index]
 
 
 class Combined(ProbaBase):
@@ -80,41 +49,46 @@ class Combined(ProbaBase):
     lr, hr, qm = combined[10]
     """
     def __init__(self, data_dir):
-        super(Combined, self).__init__(data_dir)
-        self.low_res = LowRes(data_dir)
-        self.high_res = HighRes(data_dir)
-        self.qm = QualityMaps(data_dir)
+        super(ProbaBase, self).__init__()
+        self.low_res = ProbaBase(data_dir, "LR")
+        self.high_res = ProbaBase(data_dir, "HR")
+        self.qm = ProbaBase(data_dir, "QM")
 
     def __getitem__(self, index):
         return self.low_res[index], self.high_res[index], self.qm[index]
 
 
-def subindex(shape, grid_num, x_ix, y_ix):
-    size = shape[1] / grid_num
-    y_range = int(size * y_ix), int(size * y_ix + size)
-    x_range = int(size * x_ix), int(size * x_ix + size)
-    return y_range, x_range
-
 class ProbaPatches(Combined):
+    """
+    16 x 16 (and 48 x 48) patches in Proba-V
 
+    The 16 x 16 patches are low res patches, the 48 x 48 are high res. The
+    patches are returned consecutively across images, starting at the top left
+    and marching down and to the right.
+
+    Examples
+    --------
+    data_dir = "/Users/krissankaran/Desktop/super-res/superres_data/train/NIR/"
+    patches = ProbaPatches(data_dir)
+    patches[1]
+    test = torch.utils.data.DataLoader(patches)
+    next(iter(test))
+    """
     def __init__(self, data_dir):
         super(ProbaPatches, self).__init__(data_dir)
-        self.grid_num = 8
 
     def __len__(self):
-        return 64 * len(self.hr)
+        return 64 * len(self.high_res)
 
     def __getitem__(self, index):
-        im_index = index // (self.grid_num ** 2)
-        patch_ix = index % self.grid_num
-        x_ix = patch_ix % self.grid_num
-        y_ix = patch_ix // self.grid_num
+        im_index = index // (8 ** 2)
+        patch_ix = index % 8
+        x_ix = patch_ix % 8
+        y_ix = patch_ix // 8
 
-        hr_range = subindex(self.high_res[0].shape, self.grid_num, x_ix, y_ix)
-        lr_range = subindex(self.low_res[0][0].shape, self.grid_num, x_ix, y_ix)
-        high_res = self.high_res[im_index][:, hr_range[0][0]:hr_range[0][1], hr_range[1][0]:hr_range[1][1]]
-        print(lr_range)
-        low_res = [s[:, lr_range[0][0]:lr_range[0][1], lr_range[1][0]:lr_range[1][1]] for s in self.low_res[im_index]]
-        qm = [s[:, lr_range[0][0]:lr_range[0][1], lr_range[1][0]:lr_range[1][1]] for s in self.qm[im_index]]
-
-        return low_res, high_res, qm
+        hr = self.high_res[im_index].unfold(1, 8, 8).unfold(2, 8, 8)
+        lr = [s.unfold(1, 8, 8).unfold(2, 8, 8) for s in self.low_res[im_index]]
+        lr = [s[:, :, :, x_ix, y_ix] for s in lr]
+        qm = [s.unfold(1, 8, 8).unfold(2, 8, 8) for s in self.qm[im_index]]
+        qm = [s[:, :, :, x_ix, y_ix] for s in qm]
+        return lr, hr[:, :, :, x_ix, y_ix], qm
