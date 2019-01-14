75354851945ab706ef4e93c65e57eac0d2245d23
Kris Sankaran <sankaran.kris@gmail.com>
2019-01-14 17:22:20 -0500

    functions to extract patches

--- data.py
+++ data.py
@@ -30,7 +30,7 @@ class ProbaBase(Dataset):
     def getitem_wrapper(self, index, prefix="LR"):
         paths = glob.glob(os.path.join(self.imgsets[index], prefix + "*.png"))
         result = [transforms.ToTensor()(Image.open(s)) for s in paths]
-        if len(result):
+        if len(result) == 1:
             return result[0]
 
         return result
@@ -88,3 +88,33 @@ class Combined(ProbaBase):
     def __getitem__(self, index):
         return self.low_res[index], self.high_res[index], self.qm[index]
 
+
+def subindex(shape, grid_num, x_ix, y_ix):
+    size = shape[1] / grid_num
+    y_range = int(size * y_ix), int(size * y_ix + size)
+    x_range = int(size * x_ix), int(size * x_ix + size)
+    return y_range, x_range
+
+class ProbaPatches(Combined):
+
+    def __init__(self, data_dir):
+        super(ProbaPatches, self).__init__(data_dir)
+        self.grid_num = 8
+
+    def __len__(self):
+        return 64 * len(self.hr)
+
+    def __getitem__(self, index):
+        im_index = index // (self.grid_num ** 2)
+        patch_ix = index % self.grid_num
+        x_ix = patch_ix % self.grid_num
+        y_ix = patch_ix // self.grid_num
+
+        hr_range = subindex(self.high_res[0].shape, self.grid_num, x_ix, y_ix)
+        lr_range = subindex(self.low_res[0][0].shape, self.grid_num, x_ix, y_ix)
+        high_res = self.high_res[im_index][:, hr_range[0][0]:hr_range[0][1], hr_range[1][0]:hr_range[1][1]]
+        print(lr_range)
+        low_res = [s[:, lr_range[0][0]:lr_range[0][1], lr_range[1][0]:lr_range[1][1]] for s in self.low_res[im_index]]
+        qm = [s[:, lr_range[0][0]:lr_range[0][1], lr_range[1][0]:lr_range[1][1]] for s in self.qm[im_index]]
+
+        return low_res, high_res, qm
