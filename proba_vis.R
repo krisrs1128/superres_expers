ac27858a39c9264cb37d12c41d440356bc3f112a
Kris Sankaran <sankaran.kris@gmail.com>
2019-01-18 18:57:32 -0500

    add predictions to the plot

--- proba_vis.R
+++ proba_vis.R
@@ -16,17 +16,19 @@ theme_set(
 
 ## read in all the data
 data_dir <- "/Users/krissankaran/Desktop/super-res/superres_data/train/NIR/"
-id <- "imgset1117"
+id <- "imgset1157"
 
 lr_files <- list.files(file.path(data_dir, id), "LR*", full.names = TRUE)
 hr_file <- list.files(file.path(data_dir, id), "HR*", full.names = TRUE)
 qm_files <- list.files(file.path(data_dir, id), "QM*", full.names = TRUE)
 sm_file <- list.files(file.path(data_dir, id), "SM*", full.names = TRUE)
+pred_file <- file.path("~/Desktop/train_preds", paste0(id, "_pred.png"))
 
 lr <- lapply(lr_files, readImage)
 qm <- lapply(qm_files, readImage)
 hr <- readImage(hr_file)
 sm <- readImage(sm_file)
+pred <- readImage(pred_file)
 R <- 4 # number of rows to plot
 
 ## reshape all the data into data frames
@@ -46,6 +48,9 @@ sm_df <- melt(sm[1:R, ]) %>%
   mutate(quality = as.factor(quality))
 
 hr_df <- melt(hr[seq(1, 3 * R, 3), ]) %>%
+  left_join(sm_df)
+
+pred_df <- melt(pred[seq(1, 3 * R, 3), ]) %>%
   left_join(sm_df)
 
 ## generate the plot
@@ -70,7 +75,18 @@ ggplot() +
     size = 0.5,
     shape = 2
   ) +
+  geom_point(
+    data = pred_df,
+    aes(
+      x = Var2,
+      y = value,
+      col = quality
+    ),
+    size = 0.5,
+    shape = 3
+  ) +
   scale_x_continuous(expand = c(0, 0)) +
+  xlim(0, 40) +
   guides(colour = guide_legend(override.aes = list(alpha = 1, size = 1))) +
   scale_color_manual(values = c("#cd5b45", "#5d3954")) +
   facet_grid(Var1 ~ .)
