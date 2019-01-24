7f649e61c70dda485ddc53f8bd2bd681e28d19cf
Kris Sankaran <sankaran.kris@gmail.com>
2019-01-24 13:31:51 -0500

    remove one layer

--- models/multiz_unshared.py
+++ models/multiz_unshared.py
@@ -12,9 +12,6 @@ class Block(nn.Module):
             nn.BatchNorm1d(D),
             nn.Linear(D, D),
             nn.ReLU(),
-            nn.BatchNorm1d(D),
-            nn.Linear(D, D),
-            nn.ReLU(),
             nn.BatchNorm1d(D)
         )
         self.meanvar = (nn.Linear(D, D_out), nn.Linear(D, D_out))
