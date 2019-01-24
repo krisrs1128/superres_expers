a9f428aa1a0513c6c4973391bff8080a8dc8715c
Kris Sankaran <sankaran.kris@gmail.com>
2019-01-24 11:12:08 -0500

    basically train low nad high resolution data completely separately

---
+++ models/multiz_unshared.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+import torch
+from torch import nn, optim
+from torch.nn import functional as F
+
+class Block(nn.Module):
+    def __init__(self, D_in, D_out, D=20):
+        super(Block, self).__init__()
+        self.block = nn.Sequential(
+            nn.Linear(D_in, D),
+            nn.ReLU(),
+            nn.Linear(D, D),
+            nn.ReLU()
+        )
+        self.meanvar = (nn.Linear(D, D_out), nn.Linear(D, D_out))
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class MultiZUnshared(nn.Module):
+    """
+    Simultaneously Encode / Decode Low and High-Resolution Views
+
+    The architecture separately encodes the low and high resolution views,
+    though shares a linear layer right before transforming into z. It also
+    separately decodes the two resolutions at the very end, though the encoded
+    low and high res z's are passed through the same few transformations before
+    then.
+    """
+    def __init__(self, D_low=50, D_high=200, K=20, D_block=30):
+        super(MultiZUnshared, self).__init__()
+        self.hr_encode = Block(D_high, K, D_block)
+        self.lr_encode = Block(D_low, K, D_block)
+
+        # completely disjoint encoders
+        self.encoders = {
+            "high_res": self.hr_encode.block,
+            "low_res": self.lr_encode.block
+        }
+
+        self.theta_final = {
+            "high_res": self.hr_encode.meanvar,
+            "low_res": self.lr_encode.meanvar
+        }
+
+
+        # completely disjoint decoders
+        self.hr_decode = Block(K, D_high)
+        self.lr_decode = Block(K, D_low)
+        self.decoders = {
+            "high_res": self.hr_decode.block,
+            "low_res": self.lr_decode.block
+        }
+
+        self.phi_final = {
+            "high_res": self.hr_decode.meanvar,
+            "low_res": self.lr_decode.meanvar
+        }
+
+
+    def encode(self, x, res_type="high_res"):
+        h = self.encoders[res_type](x)
+        mu, logvar = self.theta_final[res_type] # share final layer
+        return mu(h), logvar(h)
+
+
+    def decode(self, z, res_type="high_res"):
+        h = self.decoders[res_type](z)
+        mu, logvar = self.phi_final[res_type]
+        return mu(h), logvar(h)
+
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+
+    def forward(self, x_hr, x_lr):
+        # encode x into z-space: phi(x)
+        phi_low_x = self.encode(x_lr, "low_res")
+        phi_high_x = self.encode(x_hr, "high_res")
+        z_low = self.reparameterize(phi_low_x[0], phi_low_x[1])
+        z_high = self.reparameterize(phi_high_x[0], phi_high_x[1])
+
+        # decode x from z-space: theta(z)
+        theta_low_z = self.decode(z_low, "low_res")
+        theta_high_z = self.decode(z_high, "high_res")
+        return theta_low_z, phi_low_x, theta_high_z, phi_high_x
+
+
+def expected_gsn(x, theta_z):
+    sq_resid = (x - theta_z[0]) ** 2
+    return -0.5 * torch.sum(theta_z[1] + sq_resid / theta_z[1].exp())
+
+
+def gsn_kl(phi_x):
+    return - 0.5 * torch.sum(1 + phi_x[1] - phi_x[0].pow(2) - phi_x[1].exp())
+
+
+def loss_elem(x, theta_z, phi_x):
+    """
+    Reconstruction Loss for VAE
+    """
+    return - expected_gsn(x, theta_z) + gsn_kl(phi_x)
+
+
+def train_epoch(model, loader, optimizer):
+    """
+    Train Single Epoch of Model
+
+    :param model: A torch nn Module object
+    :param loader: A unwrapped DataLoader instance, each of whose elements
+      provides high and a single corresponding low resolution view.
+    :param optimizer: A torch.optim object.
+    :return A tuple containing
+      - model: The model with updated weights.
+      - optimizer: The state of the optimzier at the end of the training epoch.
+      - train_loss: The average training loss over the epoch
+    """
+    model.train()
+    train_loss = 0
+    for i, (x_hr, x_lr) in enumerate(loader):
+        optimizer.zero_grad()
+        x_hr = x_hr.flatten()
+        x_lr = x_lr.flatten()
+        theta_low_z, phi_low_x, theta_high_z, phi_high_x = model(x_hr, x_lr)
+        loss = loss_elem(x_lr, theta_low_z, phi_low_x) + \
+               loss_elem(x_hr, theta_high_z, phi_high_x)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+
+    return model, optimizer, train_loss / len(loader)
--- sandbox/apply_multiz.py
+++ sandbox/apply_multiz.py
@@ -1,18 +1,35 @@
 
-curves = CurvesUnwrapped(n_sites=1000, n_views=1, hr_size=20, lr_size=10)
+hr_size = 10
+lr_size = 5
+n_steps = 1000
+D_block = 200
+K = 200
+
+curves = CurvesUnwrapped(n_sites=50, n_views=1, hr_size=hr_size, lr_size=lr_size)
 loader = torch.utils.data.DataLoader(curves)
-model = MultiZEncoder(D_high=40, D_low=20)
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-for i in range(50):
+model = MultiZUnshared(D_high=hr_size * 2, D_low=lr_size * 2, K=K, D_block=D_block)
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+for i in range(n_steps):
     model, optimizer, loss = train_epoch(model, loader, optimizer)
     print("i: " + str(i) + "\t" + str(loss))
 
-x_hr, x_lr = curves[0]
-_, _, theta_z, _ = model(x_hr.flatten(), x_lr.flatten())
 
-for j in range(10):
-    x_rec = model.reparameterize(theta_z[0], theta_z[1])
-    x_rec = x_rec.reshape((20, 2))
-    plt.scatter(x_rec[:, 0].detach().numpy(), x_rec[:, 1].detach().numpy(), s=0.5)
-plt.scatter(x_hr[:, 0].numpy(), x_hr[:, 1].numpy(), s=0.9)
-plt.show()
+# compare truth with reconstruction means
+for i in range(10):
+    x_hr, x_lr = curves[i]
+    _, _, theta_z, _ = model(x_hr.flatten(), x_lr.flatten())
+    plt.scatter(x_hr.flatten(), theta_z[0].detach(), s=2)
+
+# compare truth with sampled reconstructions
+for j in range(100):
+    x_rec = model.reparameterize(theta_z[0], theta_z[1]).detach()
+    plt.scatter(x_hr.flatten(), x_rec, s=0.1)
+
+    # x_rec = x_rec.reshape((20, 2))
+    # plt.plot(x_rec[:, 0].detach().numpy(), x_rec[:, 1].detach().numpy())
+# plt.plot(x_hr[:, 0].numpy(), x_hr[:, 1].numpy())
+# plt.show()
+
+
+for g in optimizer.param_groups:
+    g['lr'] = 1e-5
